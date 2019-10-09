6f70bba8d79471dd0b17cbd3fe26b531175c8c2b
Slave1488 <blue101blower@gmail.com>
2019-10-09 23:30:36 +0500

    '~'

--- __main__.py
+++ __main__.py
@@ -5,8 +5,12 @@ f = open("content.txt", 'r')
 content = f.read().split('\n')
 
 doc_content = [r.group(1)
-               for r in [re.match(r'///\s*(?:)\s*', c)
+               for r in [re.match(r'\s*///\s*(.*\S)\s*', c)
                          for c in content]
                if r]
+
+doc_content = [w
+               for s in doc_content
+               for w in re.findall(r'(<.*?>)', s)]
 
 print(doc_content)
