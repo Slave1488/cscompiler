c2da132a6546ddeabaae37aff1680951af21351b
Slave1488 <blue101blower@gmail.com>
2019-10-10 23:37:53 +0500

    're'

--- __main__.py
+++ __main__.py
@@ -4,27 +4,10 @@ f = open("content.txt", 'r')
 
 content = f.read()
 
-need_data = re.findall(r'([ \t\r\f\v]*///.*(?:\s*///.*)+)\n(.*)', content)
+namespace = re.search(r'namespace ([^{/s]*)', content).group(1)
 
-temp = [(re.findall(r'(?:<.*?>)|(?:[^<\s/][^<\n/]*[^<\s])|(?:[^<\s/])', d[0]), re.search(r'/S.*/S', d[1]))
-        for d in need_data]
-
-check = False
-if check:
-    print(temp)
-else:
-    for s in temp:
-        print(s)
-
-content = content.split('\n')
-
-doc_content = [r.group(1)
-               for r in [re.match(r'\s*///\s*(.*\S)\s*', c)
-                         for c in content]
-               if r]
-
-doc_content = [
-    w
-    for s in doc_content
-    for w in re.findall(r'(?:<.*?>)|(?:[^<\s][^<]*[^<\s])|(?:[^<\s])', s)
-]
+need_data = [(re.search(r'(?:\S.*\S)|(?:\S)', d[1]).group(),
+              re.findall(r'(?:<.*?>)|(?:[^<\s/][^<\n/]*[^<\s])|(?:[^<\s/])',
+                         d[0]))
+             for d in re.findall(r'([ \t\r\f\v]*///.*(?:\s*///.*)+)\n(.*)',
+                                 content)]
