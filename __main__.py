e6b97295110303407deaed053f07d63cef1d2c18
Slave1488 <blue101blower@gmail.com>
2019-10-10 13:19:07 +0500

    're'

--- __main__.py
+++ __main__.py
@@ -2,16 +2,22 @@ import re
 
 f = open("content.txt", 'r')
 
-content = f.read().split('\n')
+content = f.read()
+
+need_data = re.findall(r'([ \t\r\f\v]*///.*(?:\s*///.*)+)\n(.*)', content)
+
+for t in need_data:
+    print("{0}\n-----------------------------------\n{1}\n".format(t[0], t[1]))
+
+content = content.split('\n')
 
 doc_content = [r.group(1)
                for r in [re.match(r'\s*///\s*(.*\S)\s*', c)
                          for c in content]
                if r]
 
-doc_content = [w
-               for s in doc_content
-               for w in re.findall(r'(?:<.*?>)|(?:[^<\s][^<]*[^<\s])|(?:[^<\s])', s)]
-
-for c in doc_content:
-    print(c)
+doc_content = [
+    w
+    for s in doc_content
+    for w in re.findall(r'(?:<.*?>)|(?:[^<\s][^<]*[^<\s])|(?:[^<\s])', s)
+]
